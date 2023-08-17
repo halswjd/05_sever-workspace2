517244c0269d3c7999a9d10ab084c70da583d629
mj <minjung3229@daum.net>
2023-08-17 21:18:54 +0900

    0817

--- jspProject/src/main/java/com/kh/board/model/service/BoardService.java
+++ jspProject/src/main/java/com/kh/board/model/service/BoardService.java
@@ -6,7 +6,9 @@ import java.sql.Connection;
 import java.util.ArrayList;
 
 import com.kh.board.model.dao.BoardDao;
+import com.kh.board.model.vo.Attachment;
 import com.kh.board.model.vo.Board;
+import com.kh.board.model.vo.Category;
 import com.kh.common.model.vo.PageInfo;
 
 public class BoardService {
@@ -29,5 +31,42 @@ public class BoardService {
 		close(conn);
 		
 		return list;
+	}
+	
+	public ArrayList<Category> selectCategoryList(){
+		
+		Connection conn = getConnection();
+		
+		ArrayList<Category> list = new BoardDao().selectCategoryList(conn);
+		
+		close(conn);
+		
+		return list;
+	}
+	
+	public int insertBoard(Board b, Attachment at) {
+		
+		Connection conn = getConnection();
+		
+		int result1 = new BoardDao().insertBoard(conn, b);
+		System.out.println("@#$@#$@#$" + result1);
+		int result2 = 1; // result1,2가 전부 1이여야 커밋되어야함
+		// 근데 reulst2를 0으로 하면 if문 안타서 0임 => 커밋안됨
+		
+		if(at != null) {
+			result2 = new BoardDao().insertAttachment(conn, at);
+			
+		}
+		
+		if(result1 > 0 && result2 > 0) {
+			commit(conn);
+		}else {
+			rollback(conn);
+		}
+		
+		close(conn);
+		
+		return result1 * result2;
+		// result1이 실패해서 0되면 0이 리턴됨
 	}
 }
